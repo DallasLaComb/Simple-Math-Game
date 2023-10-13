e63c2970a079b356fc9fa00c013d25f37d868158
Dallas <dallaslacombdrive@gmail.com>
2023-10-13 01:32:03 -0400

    Made my code more dynamic | Working for random questions pre random operator | Inserts into HTML Fine too

--- js/simple-math-game.js
+++ js/simple-math-game.js
@@ -1,17 +1,21 @@
-// =================================================================
-// Inserts the Random Question into the HTML
-// Returns the Answer to the random question
-function randomQuestion() {
+// Inserts the Random Question into the HTML.
+// Returns the Answer to the random question.
+function randomQuestion(lowestNum, highestNum) {
 // randomNumber1 gets a random number between 11 and 100 (including 11 and 100)
-    let randomNumber1 = getRandomInt(11, 100);
+    let randomNumber1 = getRandomInt(lowestNum+1, highestNum);
 // randomNumber2 gets a random number between 10 and 99 (including 10 and 99)
-    let randomNumber2 = getRandomInt(10, 99);
+    let randomNumber2 = getRandomInt(lowestNum, highestNum-1);
 // Recursion to make sure that number 2 is never bigger than number 1.
     if (randomNumber2 > randomNumber1) {
-        randomQuestion();
+        console.log("IN IF STATEMENT AT SOME POINT");
+        return randomQuestion(lowestNum,highestNum);
     } else {
-    $("#question").append(randomNumber1, " - ", randomNumber2, " ?");
-    return (randomNumber1-randomNumber2);
+        console.log("IN ELSE STATEMENT");
+
+    let answer = (randomNumber1-randomNumber2);
+
+    console.log("In randQuest: " + [answer,randomNumber1,randomNumber2]);
+    return [answer, randomNumber1, randomNumber2];
     }
 }
 // Function just to simplify getting whole numbers (ints)
@@ -22,9 +26,17 @@ function getRandomInt(min, max) {
 }
 // End of generating random Question
 // =================================================================
+// Start of inserting random Answers
+// function randomAnswers(numOfAnswers){
+//     let randomAnswers = [];
+//     for(let i=0;i<numOfAnswers-1;i++){
+//         randomAnswers[i].append()
+//     }
+// }
 function mainDriver() {
-   //Inserts the question, should follow (Random Number1, (Operand), Random Number2). RandNumber1 > RandNumber2
-    randomQuestion();
+     let answer,number1,number2;
+    [answer,number1,number2]= randomQuestion(10,100);
+    $("#question").append(number1," - ",number2," ? ", answer);
 }
 
 mainDriver();
